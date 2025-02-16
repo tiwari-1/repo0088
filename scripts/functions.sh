5312f056a0583c2a50df125cb4e90ba8ea1d4b5f
Atul V Tiwari <contact@atul-tiwari.com>
2025-02-16 16:22:27 +0000

    test

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 func1 () {
+    local empty_found=false
+      for arg in "$@"; do
+    # Check if the argument is empty or contains only whitespace
+    if [[ -z "$arg" ]]; then
+      echo "Argument '$arg' is empty"
+      empty_found=true
+    fi
+  done
     echo "Printing Arg1: $1"
 }
